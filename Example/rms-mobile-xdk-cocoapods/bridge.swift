e08e48720e9a4cb2f499229d2fc05a716da82606
Abd Qayyum <qayyum.ishak@razer.com>
2022-03-14 12:13:07 +0800

    fixed swift bridge header

---
+++ Example/rms-mobile-xdk-cocoapods/bridge.swift
@@ -0,0 +1,9 @@
+//
+//  bridge.swift
+//  rms-mobile-xdk-cocoapods_Example
+//
+//  Created by Abd Qayyum on 14/03/2022.
+//  Copyright © 2022 hisyamadzha. All rights reserved.
+//
+
+import Foundation
